0d782d6ee8163f8c368ef1daf556845d15690d11
Nic <nicznet@hotmail.com>
2025-04-30 23:03:23 +0200

    More structure

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = ()=> {
     return(
-        <header><h1>Trading Journal_</h1></header>
+        <><h1>Trading Journal_</h1></>
     )
 }
 
--- src/components/Trades.jsx
+++ src/components/Trades.jsx
@@ -2,22 +2,44 @@ import React,{useState} from 'react'
 //import { v4 as uuid} from 'uuid'
 
 const Trades = ()=> {
-    const [tradecount, setTradecount] = useState(0)
-    const [rsecured, setRsecured] = useState(0)
-    const [loss, setLoss] = useState(0)
-    const [be, setBe] = useState(0)
-    const [win, setWin] = useState(0)
+
+    const [tradecount, setTradecount] = useState(85)
+    const [rsecured, setRsecured] = useState(26.62)
+    const [loss, setLoss] = useState(25)
+    const [be, setBe] = useState(39)
+    const [win, setWin] = useState(21)
 
     const [content, setContent] = useState([
+      {
+        //id: uuid(),
+        img: "/img/01.jpg",
+        asset: 'OIL',
+        result: '-0.4'
+      },
+      {
+        
+        //id: uuid(),
+        img: "/img/01.jpg",
+        asset: 'USDJPY',
+        result: '+3'
+      },
+      {
+        //id: uuid(),
+        img: "/img/01.jpg",
+        asset: 'GER40',
+        result: '-1'
+      },
         {
           //id: uuid(),
           img: "/img/01.jpg",
-          descr: 'Some txt goes her'
+          asset: 'GBPCAD',
+          result: '-1'
         },
         {
           //id: uuid(),
           img: '/img/02.jpg',
-          descr: 'Some more txt goes her'
+          asset: 'COPPER',
+          result: '-1'
         }
       ]) 
 
@@ -27,6 +49,14 @@ const Trades = ()=> {
         <h2>Number of trades: {tradecount}</h2>
         <h3>R Secured: {rsecured}</h3>
         <h3>Win: {win} - Be: {be} - Loss: {loss}</h3>
+        <h4>PnL: {rsecured * 0.5}%</h4>
+        <p>Latest trades:</p>
+        {/* mapping over our 'db' here */}
+        {content.map(itm=> (
+            <div>
+               {itm.asset} {itm.result}
+            </div>
+          ))}
         </>
     )
 }
